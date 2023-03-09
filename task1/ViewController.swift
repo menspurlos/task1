8f17d065e692c17993a55ff1ba3bf55fe5192b13
menspurlos <dima@grishkin.net>
2023-03-09 15:58:27 +0700

    Initial commit

---
+++ task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task1
+//
+//  Created by macBook on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
